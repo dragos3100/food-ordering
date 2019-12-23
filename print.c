331604cafc843c302ed1479c3daf5ce8d331e1a4
Tirziu Dragos <56274360+dragos3100@users.noreply.github.com>
2019-12-23 19:08:49 +0200

    Add files via upload

--- print.c
+++ print.c
@@ -2,10 +2,12 @@
 // Created by Dragos on 11/8/2019.
 //
 #include <stdio.h>
-void showorder(char username[20],char **drinks,char ***foodname,int foodChoice, int foodnameChoice,int drinkChoice, int cutleryChoice, char cutlery[3][15],char info[],double **prices, double *drinksPrices)
+#include "login.h"
+
+void showorder(login * a,char **drinks,char ***foodname,int foodChoice, int foodnameChoice,int drinkChoice, int cutleryChoice, char cutlery[3][15],char info[],double **prices, double *drinksPrices)
 {   printf("This is your order:\n");
     printf("-------------\n");
-    printf("-name: %s\n", username);
+    printf("-name: %s\n", a->username);
     printf("food items:\n");
     printf("dish--- %s (%.2f)\n", foodname[foodChoice][foodnameChoice], prices[foodChoice][foodnameChoice]);
     printf("drink--- %s (%.2f)\n", drinks[drinkChoice], drinksPrices[drinkChoice]);
